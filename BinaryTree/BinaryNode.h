1f07405dce93fd30214578e45d4ac27a976b509a
xardas110 <xardas110@yahoo.no>
2021-09-20 22:17:31 +0200

    Add project files.

---
+++ BinaryTree/BinaryNode.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <vector>
+#include <initializer_list>
+
+template<class T>
+class BinaryNode
+{
+	using BinaryPtr = BinaryNode<T>*;
+public:
+	explicit BinaryNode(const T data);
+	BinaryNode(const std::initializer_list<T>& list);
+
+	BinaryNode<T>* Find(const T val);
+	
+	void Insert(const T val);
+
+	void Print() const;
+	
+	void InOrder() const;
+	void PreOrder() const;
+	void PostOrder() const;
+
+	void InOrderNotRecursive();
+	void PostOrderNotRecursive();
+
+	void InOrderReverse() const;
+
+	void InOrder(std::vector<T> &container) const;
+	void InOrderReverse(std::vector<T>& container) const;
+
+	T GetData() const;
+
+private:
+	
+	T m_data{};
+	BinaryNode<T>* m_right{ nullptr };
+	BinaryNode<T>* m_left{ nullptr };
+};
+
+
+template class BinaryNode<int>;
+template class BinaryNode<char>;
+template class BinaryNode<double>;
+template class BinaryNode<float>;
---
+++ BinaryTree/BinaryTree.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "BinaryNode.h"
+
+int main()
+{
+
+	auto* bt = new BinaryNode<int>({5,3,7,6,9,5,5,2,4,3});
+
+	
+	bt->PostOrder();
+	std::cout << std::endl << std::endl << std::endl;
+	bt->PostOrderNotRecursive();
+}
+
